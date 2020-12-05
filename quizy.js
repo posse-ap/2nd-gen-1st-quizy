5794d94bb3852bc8a90019fefd28f604d67f490f
takanashiayane <ayane.050513@icloud.com>
2020-12-05 23:05:04 +0900

    お願いします

---
+++ quizy.js
@@ -0,0 +1,27 @@
+const miss1_1 = document.getElementById('miss1_1') ;
+const miss1_2 = document.getElementById('miss1_2') ;
+const seikai1_1 = document.getElementById('seikai1_1') ;
+
+miss1_1.onclick = function(){
+    // miss1_1.class ;
+    seikai1_1.classList.add("seikai") ;
+    miss1_1.classList.add("miss") ;
+    //押されたら不正解のdivを表示する
+    const answer = document.getElementById("wrong_answer");
+    answer.style.display ="block";
+    (seikai1_1 && miss1_2).classList.add("cantclick") ;
+
+}
+miss1_2.onclick = function(){
+    seikai1_1.classList.add("seikai") ;
+    miss1_2.classList.add("miss") ;
+    const answer = document.getElementById("wrong_answer");
+    answer.style.display ="block";
+    (seikai1_1 && miss1_1).classList.add("cantclick") ;
+}
+seikai1_1.onclick = function(){
+    seikai1_1.classList.add("seikai") ;
+    const answer = document.getElementById("answer");
+    answer.style.display ="block";
+    (miss1_1 && miss1_2).classList.add("cantclick") ;
+}
