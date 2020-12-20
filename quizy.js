3956df8d4536f3eab1d23cd8bd6d0e0cbd14f012
bigdragon <ryudai-anada@keio.jp>
2020-12-20 19:53:35 +0900

    お願いします

--- quizy.js
+++ quizy.js
@@ -1,20 +1,28 @@
 'use strict'
 
-const addlist = document.createElement('li');
-
-
-
 for (let i = 1; i < 4; i++) {
+    
+    const addlist = document.createElement('li');
+    addlist.id = `answereLi${i}`;
+    const addp = document.createElement('p');
+    const addp_detail = document.createElement('p');
+
     document.getElementById(`question${i}`).addEventListener('click', e => {
         if (e.target.id === `correct${i}`) {
-            changeid();
+            addclass(1);
             answere(1);
+            answere_detail();
+            changeid();
         } else if (e.target.id === `incorrect${i}-1`) {
-            changeid();
+            addclass(2);
             answere(2);
-        } else if (e.target.id === `incorrect${i}-2`) {
+            answere_detail();
             changeid();
+        } else if (e.target.id === `incorrect${i}-2`) {
+            addclass(3);
             answere(3);
+            answere_detail();
+            changeid();
         } else {
             ;
         }
@@ -26,14 +34,35 @@ for (let i = 1; i < 4; i++) {
         document.getElementById(`incorrect${i}-2`).id = `pushed${i}-3`;
     }
 
+    function addclass(clicked) {
+        if (clicked === 1) {
+            document.getElementById(`correct${i}`).classList.add('pushed-correct');
+        } else if (clicked === 2) {
+            document.getElementById(`correct${i}`).classList.add('pushed-correct');
+            document.getElementById(`incorrect${i}-1`).classList.add('pushed-incorrect');
+        } else if (clicked === 3) {
+            document.getElementById(`correct${i}`).classList.add('pushed-correct');
+            document.getElementById(`incorrect${i}-2`).classList.add('pushed-incorrect');
+        }
+        
+    }
+
     function answere(clicked) {
         if (clicked === 1) {
-            addlist.textContent = '正解！';
+            addp.textContent = '正解！';
         } else if (clicked === 2) {
-            addlist.textContent = '不正解！';
+            addp.textContent = '不正解！';
         } else if (clicked === 3) {
-            addlist.textContent = '不正解！';
+            addp.textContent = '不正解！';
         }
         document.getElementById(`question${i}`).appendChild(addlist);
+        document.getElementById(`answereLi${i}`).appendChild(addp);
     }
+
+    function answere_detail() {
+            addp_detail.textContent = `正解は「${answers[i-1]}」です！`
+            document.getElementById(`answereLi${i}`).appendChild(addp_detail);
+        }
+    
+    const answers = ['たかなわ', 'かめいど', 'こうじまち']
 }  
