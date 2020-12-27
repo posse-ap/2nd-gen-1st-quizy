ff4efc290032eab09d7a6ed03494941f3e9fd8ec
Haruka Miki <haru.m1k1@keio.jp>
2020-12-27 09:32:56 +0900

    pull request 12/26

--- quizy.js
+++ quizy.js
@@ -1,32 +1,23 @@
 'use strict';
 
-// const wrongA1 = document.getElementById('wrong1');
-// const wrongA2 = document.getElementById('wrong2');
-// const Success = document.getElementById('success');
+let choices = [
+    ['たかわ', 'こうわ', 'たかなわ'],
+    ['かめど', 'かめと', 'かめいど'],
+    ['かゆまち', 'おかちまち', 'こうじまち'],
+    ['おかどもん', 'ごせいもん', 'おなりもん'],
+    ['たたりき', 'たたら', 'とどろき'],
+    ['せきこうい', 'いじい', 'しゃくじい'],
+    ['ざっしき', 'ざっしょく', 'ぞうしき'],
+    ['ごしろちょう', 'みとちょう', 'おかちまち'],
+    ['ろっこつ', 'しこね', 'ししぼね'],
+    ['こしゃく', 'こばく', 'こぐれ']
+];
 
-// const answer1 = document.getElementById('hidden_answer1_success');
+let answerBoxAnswer = [
+    "たかなわ", "かめいど", "こうじまち", "おなりもん", "とどろき", "しゃくじい", "ぞうしき", "おかちまち", "ししぼね", "こぐれ"
+]
 
-// const seikai = document.getElementById('seikai');
-// const seikaiexp = document.getElementById('seikaiexp');
-
-// const seikaiword = document.createTextNode("正解！");
-// const fuseikaiword = document.createTextNode("不正解！");
-// const fuseikaiexp = document.createTextNode("正解は「たかなわ」です！");
-
-let choices =[
-    ['たかわ','こうわ','たかなわ'],
-    ['かめど','かめと','かめいど'],
-    ['かゆまち','おかとまち','こうじまち'],
-    ['おかどもん','ごせいもん','おなりもん'],
-    ['たたりき','たたら','とどろき'],
-    ['せきこうい','いじい','しゃくじい'],
-    ['ざっしき','ざっしょく','ぞうしき'],
-    ['ごしろちょう','みとちょう','おかちまち'],
-    ['ろっこつ','しこね','ししぼね'],
-    ['こしゃく','こばく','こぐれ']
-    ];
-
-var images = new Array()
+let images = new Array()
 images[0] = "https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png";
 images[1] = "https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png";
 images[2] = "https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png";
@@ -39,76 +30,83 @@ images[8] = "https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d
 images[9] = "https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png";
 
 
-// ループ= 問題番号の操作,選択肢の言葉,写真
-var i;
-for (i = 0; i < 10; i++) {
-    let h =
-        '<div class="monnme">'
-        + '<div class="monnme1">'
-        + `<h2>${i+1}. この地名はなんて読む？</h2>`
-        + `<img src= ${images[i]} alt="photo">`
-        + '<ul>'
-        + `<li id="wrong1">${choices[i][0]}</li>`
-        + `<li id="wrong2">${choices[i][1]}</li>`
-        + `<li id="success">${choices[i][2]}</li>`
-        + '</ul>'
-        + '<div id="hidden_answer1_success">'
-        + '<p id="seikai"></p>'
-        + '<p id="seikaiexp"></p>'
-        + '</div>'
-        + '</div>';
-    document.write(h);
-    
+// シャッフル
+
+let insideChoices = choices[k];
+var i, k, m;
+
+for (k = 0; k < choices.length; k++) {
+    const shuffle = (insideChoices) => {
+        for (m = insideChoices.length - 1; m >= 0; m--) {
+            const j = Math.floor(Math.random() * (m + 1));
+            insideChoices[m], insideChoices[j] = insideChoices[j], insideChoices[m];
+        }
+        return insideChoices;
+    };
+    console.log(shuffle(insideChoices));
 };
 
+    for (i = 0; i < choices.length; i++) {
+        shuffle(insideChoices);
 
-const wrongA1 = document.getElementById('wrong1');
-const wrongA2 = document.getElementById('wrong2');
-const success = document.getElementById('success');
+            var answerNumber = 3;
+            // この３に答えが来るようにする
 
-const answer1 = document.getElementById('hidden_answer1_success');
+            let h =
+                '<div class="monnme">'
+                + '<div class="monnme1">'
+                + `<h2>${i + 1}. この地名はなんて読む？</h2>`
+                + `<img src= ${images[i]} alt="photo">`
+                + '<ul>'
+                + `<li id="option${i}-1" onclick="clickedFunction(${i},'${choices[i][0]}','${answerBoxAnswer[i]}',1,${answerNumber})">${choices[i][0]}</li>`
+                + `<li id="option${i}-2" onclick="clickedFunction(${i},'${choices[i][1]}','${answerBoxAnswer[i]}',2,${answerNumber})">${choices[i][1]}</li>`
+                + `<li id="option${i}-3" onclick="clickedFunction(${i},'${choices[i][2]}','${answerBoxAnswer[i]}',3,${answerNumber})">${choices[i][2]}</li>`
+                + '</ul>'
+                + `<div id="answerBox${i}">`
+                + `<p id="seikai${i}"></p>`
+                + `<p id="seikaiexp${i}"></p>`
+                + '</div>'
+                + '</div>';
+            document.write(h);
+            // let answer = value;
+        };
 
-const seikai = document.getElementById('seikai');
-const seikaiexp = document.getElementById('seikaiexp');
 
-const seikaiword = document.createTextNode(" 正解！");
-const fuseikaiword = document.createTextNode("不正解！");
-const fuseikaiexp = document.createTextNode("正解は「たかなわ」です！");
+        // shuffle 選択肢  answerの指定　abcのどれかどうか
 
-// TOD0
-// 解答ボックスに対してcssを対応させる
-// 1問目だけでなく全ての問題にaddEventListener反映させるようにする
-wrongA1.addEventListener('click', () => {
-    wrongA1.classList.add('red');
-    success.classList.add('blue');
-    wrongA1.classList.add('cantclick');
-    wrongA2.classList.add('cantclick');
-    success.classList.add('cantclick');
-    answer1.classList.add('hidden_answer1_success');
-    seikai.appendChild(fuseikaiword);
-    seikai.classList.add('redbar');
-    seikaiexp.appendChild(fuseikaiexp);
-});
+        var clickedFunction = function (rounds, clkdButton, answer, pressed, answerNumber) {
+            // クリックされたのがどれか、どれが答えか
+            let option1 = document.getElementById('option' + rounds + '-1');
+            let option2 = document.getElementById('option' + rounds + '-2');
+            let option3 = document.getElementById('option' + rounds + '-3');
 
-wrongA2.addEventListener('click', () => {
-    wrongA2.classList.add('red');
-    success.classList.add('blue');
-    wrongA1.classList.add('cantclick');
-    wrongA2.classList.add('cantclick');
-    success.classList.add('cantclick');
-    answer1.classList.add('hidden_answer1_success');
-    seikai.appendChild(fuseikaiword);
-    seikai.classList.add('redbar');
-    seikaiexp.appendChild(fuseikaiexp);
-});
+            let answerBox = document.getElementById('answerBox' + rounds);
+            let answer1 = document.getElementById('option' + rounds + '-' + answerNumber);
+            let pressedButton = document.getElementById('option' + rounds + '-' + pressed);
 
-success.addEventListener('click', () => {
-    success.classList.add('blue');
-    wrongA1.classList.add('cantclick');
-    wrongA2.classList.add('cantclick');
-    success.classList.add('cantclick');
-    answer1.classList.add('hidden_answer1_success');
-    seikai.appendChild(seikaiword);
-    seikai.classList.add('bluebar');
-    seikaiexp.appendChild(fuseikaiexp);
-});
+
+            let seikai = document.getElementById('seikai' + rounds);
+            let seikaiexp = document.getElementById('seikaiexp' + rounds);
+
+            let seikaiword = document.createTextNode(" 正解！");
+            let fuseikaiword = document.createTextNode("不正解！");
+            let fuseikaiexp = document.createTextNode(`正解は「${answerBoxAnswer[rounds]}」です！`);
+
+            option1.classList.add('cantclick');
+            option2.classList.add('cantclick');
+            option3.classList.add('cantclick');
+            answerBox.classList.add('answerBox');
+
+            seikaiexp.appendChild(fuseikaiexp);
+            answer1.classList.add('successButton');
+
+            if (answer === clkdButton) {
+                seikai.appendChild(seikaiword);
+                seikai.classList.add('successBar');
+            } else {
+                pressedButton.classList.add('wrongButton');
+                seikai.appendChild(fuseikaiword);
+                seikai.classList.add('wrongBar');
+            }
+        };
+    
