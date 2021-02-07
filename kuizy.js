0ba8cca93cce3dce6afba5a7d464a4e2fd0cc283
MayuNA <mayuchaichai@gmail.com>
2021-02-07 20:07:31 +0900

    よろしくお願いします！

--- kuizy.js
+++ kuizy.js
@@ -1,191 +1,128 @@
 'use strict' ;
 
-// const btn1 = document.getElementById('button1');
-// const btn2 = document.getElementById('button2');
-// const btn3 = document.getElementById('button3');
-
-// const TorF = document.getElementById('TorF');
-// const true_is = document.getElementById('true_is');
-// const success1 = document.createTextNode("正解!");
-// const false1 = document.createTextNode("不正解！");
-// const success2 = document.createTextNode("正解は『たかなわ』です！");
-// const target = document.getElementById('target');
-// const image_box = document.getElementById("image_box");
-// const loop1 = document.getElementById("loop1");
-
-// // const click_space = document.getElementById("click_space");
-// // var img = document.getElementById('image_place');
-
-// btn1.onclick = () => { 
-//     btn1.classList.add("red");
-//     btn2.classList.add("blue");
-//     TorF.appendChild(false1);
-//     true_is.appendChild(success2);
-//     target.classList.add("box");
-//     TorF.classList.add("ansF");
-//     button1.classList.add("finish");
-//     button2.classList.add("finish");
-//     button3.classList.add("finish");
-// };
-
-// btn2.onclick = () => {
-//     btn2.classList.add("blue");
-//     TorF.appendChild(success1);
-//     true_is.appendChild(success2);
-//     target.classList.add("box");
-//     TorF.classList.add("ansT");
-//     button1.classList.add("finish");
-//     button2.classList.add("finish");
-//     button3.classList.add("finish");
-// };
-
-// btn3.onclick = () => {
-//     btn2.classList.add("blue");
-//     btn3.classList.add("red");
-//     TorF.appendChild(false1);
-//     true_is.appendChild(success2);
-//     target.classList.add("box");
-//     TorF.classList.add("ansF");
-//     button1.classList.add("finish");
-//     button2.classList.add("finish");
-//     button3.classList.add("finish");
-// };
-
-const answer_choices =[
-     ['たかわ','たかなわ','こうわ'],
-     ['かめいど','かめと','かめど'],
-     ['おかちまち','こうじまち','かゆまち'],
-     ['j','k','l'],
-     ['m','n','o'],
-     ['p','q','r'],
-     ['s','t','u'],
-     ['v','w','x'],
-     ['y','z','A'],
-     ['B','C','D'],
+const question_list = [
+['たかなわ','こうわ','たかわ'],
+['かめいど','かめと','かめど'],
+['こうじまち','おかとまち','かゆまち'],
+['おなりもん','おかどもん','ごせいもん'],
+['とどろき','たたりき','たたら'],
+['しゃくじい','せきこうい','いじい'],
+['ぞうしき','ざっしょく','ざっしき'],
+['おかちまち','みとちょう','ごしろちょう'],
+['ししぼね','ろっこつ','しこね'],
+['こぐれ','こしゃく','こばく'],
 
 ];
-// ]).map(shuffle);
 
 
-let main_contents;
+// let seikai = ['たかなわ','かめいど','こうじまち','おなりもん','とどろき','しゃくじい','ぞうしき','おかちまち','ししぼね','こぐれ'];
 
 
-for( var i= 0;i<10;i++){
-    const TorF = document.getElementById('TorF');
-    const true_is = document.getElementById('true_is');
-    const success1 = document.createTextNode("正解!");
-    const false1 = document.createTextNode("不正解！");
-    const success2 = document.createTextNode("正解は『たかなわ』です！");
-    const target = document.getElementById('target');
-    const image_box = document.getElementById("image_box");
-    let btn1 = document.getElementById(`button${i*3+1}`);
-    let btn2 = document.getElementById(`button${i*3+2}`);
-    let btn3 = document.getElementById(`button${i*3+3}`); 
-    const loop1 = document.getElementById("loop1");
-    // kokodayo.innerHTML='<span class="under_line">1.この地名はなんて読む？</span>';
-    var img_src = i + ".png";
+const image_box = document.getElementById("image_box");
+const loop1 = document.getElementById("loop1");
+
+
+function createquestion(question_id,each_question,true_id){
+
+    var main_contents = [];
+
+    main_contents += '<div id ="image_box"></div>';
+    var img_src = (question_id-1) + ".png";
     var img_tag = '<img src="' + img_src + '">';
     console.log(img_tag);
-    image_box.innerHTML=img_tag;
+    main_contents += '<span class="under_line">' + question_id +'.この地名はなんて読む？</span>' + img_tag  +'<ul>';
+
+    each_question.forEach(function(item,index){
+        main_contents += ' <li class="button" id ="answerlist_' + question_id + '_' + (index + 1) +'"name="answerlist_' + question_id + '" onclick="check('+ question_id + ', ' + (index + 1) + ', ' + true_id +')">' + item + '</li>' ;
+        
+        console.log("ok2");
+
+    });
+    main_contents += '<div id="answerbox_' + question_id + '" class="box"><p id = "answertext_' + question_id +'"></p><br> <p>正解は「' + each_question[true_id - 1] + '」です！</p> </div>';
+
+    document.getElementById('loop1').insertAdjacentHTML('beforeend',main_contents);
     
-    main_contents += '<span class="under_line">' + [i+1] +'.この地名はなんて読む？</span>' + img_tag  +'<ul>';
     
-    for (var j=0;j<3;j++){
-        btn1.onclick =() =>　{ console.log("ok");
-            btn1.classList.add("red");
-            btn2.classList.add("blue");
-            TorF.appendChild(false1);
-            true_is.appendChild(success2);
-            target.classList.add("box");
-            TorF.classList.add("ansF");
-            button1.classList.add("finish");
-            button2.classList.add("finish");
-            button3.classList.add("finish"); 
-        };
+}
 
-        btn2.onclick = () => {
-            btn2.classList.add("blue");
-            TorF.appendChild(success1);
-            true_is.appendChild(success2);
-            target.classList.add("box");
-            TorF.classList.add("ansT");
-            button1.classList.add("finish");
-            button2.classList.add("finish");
-            button3.classList.add("finish");
-        };
 
-        btn3.onclick = () => {
-            btn2.classList.add("blue");
-            btn3.classList.add("red");
-            TorF.appendChild(false1);
-            true_is.appendChild(success2);
-            target.classList.add("box");
-            TorF.classList.add("ansF");
-            button1.classList.add("finish");
-            button2.classList.add("finish");
-            button3.classList.add("finish");
-        };
-        // var id_name = `button${j+1}`;
-        // var id_NAME = '<id ="' + id_name +'">';
-        // console.log(id_NAME)
-        main_contents += `<li class="button" id ="button${i*3+j+1}" >` + answer_choices[i][j] + '</li>' ;
-    };
+
     
-      main_contents += '</ul>';
-      loop1.innerHTML = main_contents;
+function createhtml(){
+    question_list.forEach(function(question,index){
 
-    // let btn1 = document.getElementById(`button${i*3+1}`);
-    // let btn2 = document.getElementById(`button${i*3+2}`);
-    // let btn3 = document.getElementById(`button${i*3+3}`);
-    
 
-        // btn1.onclick =function (){ console.log("ok");
-        //     btn1.classList.add("red");
-        //     btn2.classList.add("blue");
-        //     TorF.appendChild(false1);
-        //     true_is.appendChild(success2);
-        //     target.classList.add("box");
-        //     TorF.classList.add("ansF");
-        //     button1.classList.add("finish");
-        //     button2.classList.add("finish");
-        //     button3.classList.add("finish"); 
-        // };
+        const answer = question[0];
 
-        // btn2.onclick = () => {
-        //     btn2.classList.add("blue");
-        //     TorF.appendChild(success1);
-        //     true_is.appendChild(success2);
-        //     target.classList.add("box");
-        //     TorF.classList.add("ansT");
-        //     button1.classList.add("finish");
-        //     button2.classList.add("finish");
-        //     button3.classList.add("finish");
-        // };
+        console.log('1'+ question);
 
-        // btn3.onclick = () => {
-        //     btn2.classList.add("blue");
-        //     btn3.classList.add("red");
-        //     TorF.appendChild(false1);
-        //     true_is.appendChild(success2);
-        //     target.classList.add("box");
-        //     TorF.classList.add("ansF");
-        //     button1.classList.add("finish");
-        //     button2.classList.add("finish");
-        //     button3.classList.add("finish");
-        // };
-    // let btn1 = document.getElementById(`button${i*3+j+1}`);
-    // btn1.onclick = () => { 
-    //     btn1.classList.add("red");
-    //     btn2.classList.add("blue");
-    //     TorF.appendChild(false1);
-    //     true_is.appendChild(success2);
-    //     target.classList.add("box");
-    //     TorF.classList.add("ansF");
-    //     button1.classList.add("finish");
-    //     button2.classList.add("finish");
-    //     button3.classList.add("finish");
-    
-};
-// click_space.innerHTML = id_NAME;
+        const shuffle = ([...array]) => {
+            for(let i = array.length - 1; i>0;i--){
+                const j = Math.floor(Math.random() * (i+1));
+                [array[j],array[i]] = [array[i],array[j]];
 
-// loop1.innerHTML = main_contents;
+            }
+        return array
+        }
+
+        var after_shuffle = shuffle(question);
+
+        console.log(after_shuffle);
+
+        // console.log('2' + question);
+        
+            createquestion(index+1,after_shuffle,after_shuffle.indexOf(answer) + 1);
+            //index+1 = question_id
+            //indexOfはanswerの場所を探すため
+        })
+
+
+}
+
+function check(question_id,click_id,true_id){
+
+    //クリック無効化
+    var answerlists = document.getElementsByName('answerlist_' + question_id);
+    answerlists.forEach(answerlist => {
+        answerlist.style.pointerEvents = 'none';
+    });
+
+   
+
+    var button_red = document.getElementById('answerlist_' + question_id + '_' + click_id);
+    var button_blue = document.getElementById('answerlist_' + question_id + '_' + true_id);
+    // selectiontext.classList.add('red');
+    button_blue.classList.add('blue');
+ 
+
+    var success1 = document.createTextNode('正解!');
+    var false1 = document.createTextNode('不正解!');
+    var answerbox = document.getElementById('answerbox_' + question_id);
+    var answertext = document.getElementById('answertext_' + question_id);
+    // var num = Number(click_id);
+    // console.log('num' + num);
+    // var key = Math.floor(num / 11);
+    // console.log('key:' + key);
+
+    if (click_id == true_id) {
+        console.log('正解');
+        // answertext.classList.add('blue');
+        answertext.appendChild(success1);
+        answertext.classList.add('ansT');
+        answertext.classList.add('syutugenn')
+    } else {
+         console.log('不正解');
+         button_red.classList.add('red');
+         answertext.classList.add('ansF');
+        //  answertext.classList.add('red');
+         answertext.appendChild(false1);
+         answertext.classList.add('syutugenn')
+    }
+   answerbox.style.display = 'block';
+}
+
+
+
+
+window.load = createhtml();
+// loop1.innerHTML += main_contents;
