5230e03845ae4ff5b4903309f753b735c649d68c
serentada <serenade65@icloud.com>
2021-02-14 19:15:41 +0900

    よろしくお願いします

--- quizy2.js
+++ quizy2.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const choices = ([
+const choices = [
     "たかなわ","かめいど","こうじまち","おなりもん","とどろき","しゃくじい","ぞうしき","おかちまち","ししぼね","こぐれ"
-]);
-const choices2 = ([
-    ["たかなわ","こうわ","たかわ"],
+];
+const choices2 = [
+    ["たかなわ","こうわ","たかわ",],
     ["かめいど","かめと","かめど"],
     ["こうじまち","おかとまち","かゆまち"],
     ["おなりもん","おかどもん","ごせいもん"],
@@ -14,20 +14,37 @@ const choices2 = ([
     ["おかちまち","みとちょう","ごしろちょう"],
     ["ししぼね","しこね","ろっこつ"],
     ["こぐれ","こばく","こしゃく"],
-]);
+];
 
-function shuffle(sentakusi) {
-    for (let k = sentakusi.length - 1; k > 0; k--) { 
-    const j = Math.floor(Math.random() * (k + 1));  
-    [sentakusi[j], sentakusi[k]] = [sentakusi[k], sentakusi[j]];
-    }
-    return sentakusi;
-};
+// function shuffle(arrey) {
+//     for (let k = arrey.length - 1; k > 0; k--) { 
+//     const j = Math.floor(Math.random() * (k + 1));  
+//     [arrey[j], arrey[k]] = [arrey[k], arrey[j]];
+//     }
+//     return arrey;
+// };
+// choices2.map(shuffle);
 
-choices2.map(shuffle);
+var newestArray=[]
+ for (let i = 0  ; i < 10 ; i++){
+   var array = [1,2,0];
+   var newArray = [];
+   var j;
+   var k; 
+
+  while (array.length > 0) {
+   j = array.length;
+   k = Math.floor(Math.random() * j); 
+
+   newArray.push (array[k]);
+   array.splice(k, 1);
+
+ }
+  newestArray.push(newArray)
+ }
 
 var b = [];
-for(let i =0; i <choices2.length; i++){
+for(let i =0; i < choices2.length; i++){
     var a = choices2[i].indexOf(choices[i]);
     b.push(a);
 };
@@ -51,7 +68,7 @@ for(let i = 0; i < choices.length; i++){
         + '</div>';
     document.write(h);
 };
- 
+
 function ignition(id,number,seikai){
     let matigai    = document.getElementById('matigai' + id + '-' + number);
     let success    = document.getElementById('matigai' + id + '-' + seikai);
@@ -62,25 +79,25 @@ function ignition(id,number,seikai){
     const trueis   = document.getElementById(`trueis` + id);
     const target   = document.getElementById(`target` + id);
     const success1 = document.createTextNode("正解！");
-    const success2 = document.createTextNode(`正解は『${choices[id]}』です！`);
+    const success2 = document.createTextNode(`正解は「${choices[id]}」です！`);
     const failure1 = document.createTextNode("不正解！");
     // let scroll     = target.element.getBoundingClientRect();
     // let position   = scroll.top;
 
     if (number == seikai){                 
         TorF.appendChild(success1);
-        trueis.appendChild(success2);
         TorF.classList.add("ansT");
     }
-    else {
+    else { 
         matigai.classList.add("red");
         TorF.appendChild(failure1);
-        trueis.appendChild(success2);
         TorF.classList.add("ansF");
     };
     // scrollBy(0,position);
-    success.classList.add("blue");
+    
     target.classList.add("target");
+    trueis.appendChild(success2);
+    success.classList.add("blue");
     matigai1.classList.add("cantclick");
     matigai2.classList.add("cantclick");
     matigai3.classList.add("cantclick");
